f07cac2e5527ab1aa2167421d7e1046c1d7fe3be
infinity <support@infisms.co.in>
2025-12-30 00:54:02 +0530

    chnages base url

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_BASE_URL,
+    baseUrl: import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:5000',
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as any)?.auth?.accessToken;
 
@@ -20,7 +20,7 @@ export const baseApi = createApi({
 export const publicApi = createApi({
   reducerPath: 'publicApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_BASE_URL
+    baseUrl: import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:5000'
   }),
   tagTypes: [],
   endpoints: () => ({}),
